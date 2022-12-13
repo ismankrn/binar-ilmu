1af3760e868491bdc5dc5caa0a0e2468dab07b2c
Isman Kurniawan <isman.krn@gmail.com>
2022-12-13 13:37:02 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1781,5 +1781,6 @@ def add_header(r):
     r.headers['Cache-Control'] = 'public, max-age=0'
     return r
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
